6ed2b40a0f8b6d191331be47d7a24c0640203905
Martin Ettl <ettl.martin78@googlemail.com>
2013-06-19 11:35:04 +0200

    extended english dictionry file; removed not needed script.

--- checkAVRcdRom.sh
+++
@@ -1,5 +0,0 @@
-cat dictionary.txt > dictionary_tmp.txt &&\
-cat dictionary_en.txt >> dictionary_tmp.txt &&\
-cat dictionary_tmp.txt | sort | uniq > dictionary_all.txt &&\
-rm dictionary_tmp.txt &&\
-./codespell.py -d -R --skip *.lss,*.patch,*.gcov,*.svn*,*.html,*.png,*.o,*.db,*.spl,*.so,*.dpi,*.lock,*.dyn,*.gcno,*.gcda,*HTM*,*.bck,*.map,*~ dictionary_all.txt  /media/martin/Mikrocontroller/ > t
