5481f397661fea90e7cc1d3f487205ce25995da1
orbitcowboy <ettl.martin78@gmail.com>
2013-12-07 00:22:34 -0800

    updated sorting script

--- sortDictionaries.sh
+++ sortDictionaries.sh
@@ -1,13 +1,13 @@
-#/bin/bash
-# sort the english dictionary
+#!/bin/bash
+# sort the english dictionary file
 cat dictionary_en.txt > dictionary_tmp.txt
 cat dictionary_tmp.txt | sort | uniq > dictionary_en.txt 
 rm dictionary_tmp.txt
-# sort the german dictionary
+# sort the german dictionary file
 cat dictionary_de.txt > dictionary_tmp.txt
 cat dictionary_tmp.txt | sort | uniq > dictionary_de.txt 
 rm dictionary_tmp.txt
-# sort the german dictionary
+# sort the generated dictionary file
 cat dictionary_generated.txt > dictionary_tmp.txt
 cat dictionary_tmp.txt | sort | uniq > dictionary_generated.txt 
 rm dictionary_tmp.txt
