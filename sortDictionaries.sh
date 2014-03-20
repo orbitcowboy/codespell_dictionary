ddc7bca5cdb0b103a6af231a913d7dc1615cc1fd
orbitcowboy <ettl.martin78@gmail.com>
2014-03-20 13:16:48 +0100

    SortDictionary.sh: remove trailing white spaces

--- sortDictionaries.sh
+++ sortDictionaries.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # sort the english dictionary file
 cat dictionary_en.txt > dictionary_tmp.txt
-cat dictionary_tmp.txt | sort | uniq > dictionary_en.txt 
+cat dictionary_tmp.txt | sort | uniq > dictionary_en.txt
+# remove trailing blanks on each line
+sed 's/[[:blank:]]*$//' -i dictionary_en.txt
 rm dictionary_tmp.txt
 # sort the german dictionary file
 cat dictionary_de.txt > dictionary_tmp.txt
-cat dictionary_tmp.txt | sort | uniq > dictionary_de.txt 
+cat dictionary_tmp.txt | sort | uniq > dictionary_de.txt
+# remove trailing blanks on each line
+sed 's/[[:blank:]]*$//' -i dictionary_de.txt
 rm dictionary_tmp.txt
 # sort the generated dictionary file
 cat dictionary_generated.txt > dictionary_tmp.txt
-cat dictionary_tmp.txt | sort | uniq > dictionary_generated.txt 
+cat dictionary_tmp.txt | sort | uniq > dictionary_generated.txt
+# remove trailing blanks on each line
+sed 's/[[:blank:]]*$//' -i dictionary_generated.txt
 rm dictionary_tmp.txt
