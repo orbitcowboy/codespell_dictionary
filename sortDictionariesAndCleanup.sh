578c718fe7dc284eea4154c946f5fd1300e0bd2a
orbitcowboy <orbitcowboy@web.de>
2016-12-14 10:41:48 +0100

    Updated script to trim dictionary entries.

--- sortDictionariesAndCleanup.sh
+++ sortDictionariesAndCleanup.sh
@@ -4,16 +4,26 @@ cat dictionary_en.txt > dictionary_tmp.txt
 cat dictionary_tmp.txt | sort | uniq > dictionary_en.txt
 # remove trailing blanks on each line
 sed 's/[[:blank:]]*$//' -i dictionary_en.txt
-rm dictionary_tmp.txt
+# convert upper case to lower case letters
+sed 's/\([A-Z]\)/\L\1/g' -i dictionary_en.txt
+# remove duplicate lines
+uniq dictionary_en.txt dictionary_tmp.txt
+mv dictionary_tmp.txt dictionary_en.txt
+
 # sort the german dictionary file
 cat dictionary_de.txt > dictionary_tmp.txt
 cat dictionary_tmp.txt | sort | uniq > dictionary_de.txt
 # remove trailing blanks on each line
 sed 's/[[:blank:]]*$//' -i dictionary_de.txt
 rm dictionary_tmp.txt
+
 # sort the generated dictionary file
 cat dictionary_generated.txt > dictionary_tmp.txt
 cat dictionary_tmp.txt | sort | uniq > dictionary_generated.txt
 # remove trailing blanks on each line
 sed 's/[[:blank:]]*$//' -i dictionary_generated.txt
-rm dictionary_tmp.txt
+# convert upper case to lower case letters
+sed 's/\([A-Z]\)/\L\1/g' -i dictionary_generated.txt
+uniq dictionary_generated.txt dictionary_tmp.txt
+# remove duplicate lines
+mv dictionary_tmp.txt dictionary_generated.txt
