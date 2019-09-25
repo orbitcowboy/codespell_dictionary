cb97886077cac3152e9da8bb37b9daf6ddcd89b8
orbitcowboy <orbitcowboy@web.de>
2019-09-25 12:01:24 +0200

    Extended the Englisch dictionary file.

--- sortDictionariesAndCleanup.sh
+++ sortDictionariesAndCleanup.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 WORKINGDIR=$(dirname $(readlink -f $0))
-cp ${WORKINGDIR}/dictionary_all.txt ${WORKINGDIR}/dictionary_en.txt 
+cp ${WORKINGDIR}/dictionary_all.txt ${WORKINGDIR}/dictionary_tmp.txt 
 # include dictionary from codespell
 cd ${WORKINGDIR}/codespell_git/codespell/
 git stash
@@ -17,3 +17,6 @@ sed 's/[[:blank:]]*$//' -i ${WORKINGDIR}/dictionary_all.txt
 sed 's/\([A-Z]\)/\L\1/g' -i ${WORKINGDIR}/dictionary_all.txt
 # Remove duplicate entries -i e.g.: abc->abc
 sed -E '/^(.*)->\1$/d' -i ${WORKINGDIR}/dictionary_all.txt
+# Sort and remove duplicate entries
+cat ${WORKINGDIR}/dictionary_all.txt | sort | uniq >  ${WORKINGDIR}/dictionary_tmp.txt 
+mv ${WORKINGDIR}/dictionary_tmp.txt ${WORKINGDIR}/dictionary_all.txt 
