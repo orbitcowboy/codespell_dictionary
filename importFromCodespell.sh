d778933caf9508ef02579b23eebc2d363c6ecbb3
orbitcowboy <orbitcowboy@web.de>
2019-09-28 14:32:50 +0200

    Extended the Englisch dictionary file.

---
+++ importFromCodespell.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+set -e
+WORKINGDIR=$(dirname $(readlink -f $0))
+cp ${WORKINGDIR}/dictionary_all.txt ${WORKINGDIR}/dictionary_tmp.txt 
+# include dictionary from codespell
+cd ${WORKINGDIR}/codespell_git/codespell/
+git stash
+git pull
+make sort-dictionary
+cat ${WORKINGDIR}/codespell_git/codespell/codespell_lib/data/dictionary.txt >> ${WORKINGDIR}/dictionary_tmp.txt
+cd ${WORKINGDIR}
+cat ${WORKINGDIR}/dictionary_tmp.txt | sort | uniq > ${WORKINGDIR}/dictionary_all.txt
--- sortDictionariesAndCleanup.sh
+++ sortDictionariesAndCleanup.sh
@@ -2,15 +2,6 @@
 set -x
 set -e
 WORKINGDIR=$(dirname $(readlink -f $0))
-cp ${WORKINGDIR}/dictionary_all.txt ${WORKINGDIR}/dictionary_tmp.txt 
-# include dictionary from codespell
-cd ${WORKINGDIR}/codespell_git/codespell/
-git stash
-git pull
-make sort-dictionary
-cat ${WORKINGDIR}/codespell_git/codespell/codespell_lib/data/dictionary.txt >> ${WORKINGDIR}/dictionary_tmp.txt
-cd ${WORKINGDIR}
-cat ${WORKINGDIR}/dictionary_tmp.txt | sort | uniq > ${WORKINGDIR}/dictionary_all.txt
 # remove trailing blanks on each line
 sed 's/[[:blank:]]*$//' -i ${WORKINGDIR}/dictionary_all.txt
 # convert upper case to lower case letters
