5c98ab8872cc97e04e51818428b2a54d505f0cc5
orbitcowboy <orbitcowboy@web.de>
2019-10-09 10:34:30 +0200

    sortDictionariesAndCleanup.sh: Enforce sorting 'LC_ALL=en_US' to avoid portability issues.

--- sortDictionariesAndCleanup.sh
+++ sortDictionariesAndCleanup.sh
@@ -9,5 +9,5 @@ sed 's/\([A-Z]\)/\L\1/g' -i ${WORKINGDIR}/dictionary_all.txt
 # Remove duplicate entries -i e.g.: abc->abc
 sed -E '/^(.*)->\1$/d' -i ${WORKINGDIR}/dictionary_all.txt
 # Sort and remove duplicate entries
-cat ${WORKINGDIR}/dictionary_all.txt | sort | uniq >  ${WORKINGDIR}/dictionary_tmp.txt 
+cat ${WORKINGDIR}/dictionary_all.txt | LC_ALL=en_US sort | uniq >  ${WORKINGDIR}/dictionary_tmp.txt 
 mv ${WORKINGDIR}/dictionary_tmp.txt ${WORKINGDIR}/dictionary_all.txt 
