da8522902a1c38a801cb508c592587074c079c1a
orbitcowboy <orbitcowboy@web.de>
2019-10-14 10:33:24 +0200

    importFromCodespell.sh: Fixed broken script.

--- importFromCodespell.sh
+++ importFromCodespell.sh
@@ -4,6 +4,10 @@ set -e
 WORKINGDIR=$(dirname $(readlink -f $0))
 cp ${WORKINGDIR}/dictionary_all.txt ${WORKINGDIR}/dictionary_tmp.txt 
 # include dictionary from codespell
+rm -fR ${WORKINGDIR}/codespell_git
+mkdir ${WORKINGDIR}/codespell_git
+cd ${WORKINGDIR}/codespell_git
+git clone https://github.com/codespell-project/codespell.git codespell
 cd ${WORKINGDIR}/codespell_git/codespell/
 git stash
 git pull
@@ -11,3 +15,4 @@ make sort-dictionary
 cat ${WORKINGDIR}/codespell_git/codespell/codespell_lib/data/dictionary.txt >> ${WORKINGDIR}/dictionary_tmp.txt
 cd ${WORKINGDIR}
 cat ${WORKINGDIR}/dictionary_tmp.txt | LC_ALL=en_US sort | uniq > ${WORKINGDIR}/dictionary_all.txt
+bash sortDictionariesAndCleanup.sh
