1a24c424271106f46410451b4b1f2e6fbb779f44
orbitcowboy <orbitcowboy@web.de>
2021-05-14 13:47:05 +0200

    Fixed syntax errors in dictionary added a script check

--- sortDictionariesAndCleanup.sh
+++ sortDictionariesAndCleanup.sh
@@ -16,6 +16,15 @@ sed -E '/^(.*)->\1$/d' -i ${WORKINGDIR}/dictionary_all.txt
 cat ${WORKINGDIR}/dictionary_all.txt | LC_ALL=en_US sort | uniq >  ${WORKINGDIR}/dictionary_tmp.txt 
 mv ${WORKINGDIR}/dictionary_tmp.txt ${WORKINGDIR}/dictionary_all.txt 
 
+# Check for syntax errors
+TMP_FILE_PATH=/tmp/syntaxErrors.txt
+grep -Pv '[-][>]' dictionary_all.txt > "$TMP_FILE_PATH" || true
+if [[ -f "$TMP_FILE_PATH" && -s "$TMP_FILE_PATH" ]]; then
+	echo -e "\e[91mSyntax error in dictionary_all\e[39m";
+	cat "$TMP_FILE_PATH";
+fi
+rm -f "$TMP_FILE_PATH"
+
 # GER-dictionary:
 
 # remove trailing blanks on each line
@@ -25,3 +34,11 @@ sed -E '/^(.*)->\1$/d' -i ${WORKINGDIR}/dictionary_de.txt
 # Sort and remove duplicate entries
 cat ${WORKINGDIR}/dictionary_de.txt | LC_ALL=de_DE sort | uniq >  ${WORKINGDIR}/dictionary_tmp.txt
 mv ${WORKINGDIR}/dictionary_tmp.txt ${WORKINGDIR}/dictionary_de.txt
+
+# Check for syntax errors
+grep -Pv '[-][>]' dictionary_de.txt > "$TMP_FILE_PATH" || true
+if [[ -f "$TMP_FILE_PATH" && -s "$TMP_FILE_PATH" ]]; then
+	echo -e "\e[91mSyntax error in dictionary_de\e[39m";
+	cat "$TMP_FILE_PATH"
+fi
+rm -f "$TMP_FILE_PATH"
